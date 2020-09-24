8f1a97fd501fe5b21c5f754a46fc3be62687aa0c
Jay Phelps <jay@jayphelps.name>
2020-09-23 21:44:10 -0500

    Adjusted the start for Heroku

--- src/server/start.js
+++ src/server/start.js
@@ -1,6 +1,7 @@
 // start.js - starts the server
-
+// updated from:
+// https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
 const serverport = 5010;
 const app = require('../server/server');
-app.listen(serverport);
+app.listen(listen(process.env.PORT || serverport));
 console.log(`Travel App Server serving ${__dirname} and listening on port ${serverport} ...`);
