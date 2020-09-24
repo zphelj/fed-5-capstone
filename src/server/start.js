0d1bd327be02749ec62de16074e4ec904e74901d
Jay Phelps <jay@jayphelps.name>
2020-09-24 10:58:06 -0500

    Fixed typo preventing start.js from executing.

--- src/server/start.js
+++ src/server/start.js
@@ -3,5 +3,5 @@
 // https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
 const serverport = 5010;
 const app = require('../server/server');
-app.listen(listen(process.env.PORT || serverport));
+app.listen(process.env.PORT || serverport);
 console.log(`Travel App Server serving ${__dirname} and listening on port ${serverport} ...`);
